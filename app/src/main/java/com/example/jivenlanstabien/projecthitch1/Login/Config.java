de2c7b822de17abdd224dd8f4476450759133f1d
Carlo Ortega <carlopaoloortega@gmail.com>
2016-06-13 08:55:05 -0700

    Carlo Ortega consolidated - Reset password

--- app/src/main/java/com/example/jivenlanstabien/projecthitch1/Login/Config.java
+++ app/src/main/java/com/example/jivenlanstabien/projecthitch1/Login/Config.java
@@ -1,12 +1,20 @@
 package com.example.jivenlanstabien.projecthitch1.Login;
 
 /**
- * Created by jivenlans on 6/8/2016.
+ * Created by Carlo Ortega on 6/1/2016.
  */
 public class Config {
     public static final String URL_GET_USER_LOGIN = "http://angkas.net23.net/getuserlogin.php?LoginID=";
+    public static final String URL_GET_SECURITY_CODE = "http://angkas.net23.net/getsecuritycode.php?Code=";
+    public static final String URL_DELETE_SECURITY_CODE = "http://angkas.net23.net/deletesecuritycode.php?Code=";
+
     public static final String KEY_LOGIN_ID = "LoginID";
-    public static final String TAG_TR_JSON_ARRAY ="result";
+    public static final String TAG_LOGINID_JSON_ARRAY ="result";
+
+    public static final String KEY_SECURITY_CODE = "Code";
+    public static final String TAG_CODE_JSON_ARRAY ="result";
+
+
 
     public static final String EMAIL ="carlopaoloortega@gmail.com";
     public static final String PASSWORD ="09195795408";
